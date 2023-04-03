b35af5dc9289e422ae3d3b84b7a14b0500ef5ce4
Jhonatan Henkel <jhowrf@gmail.com>
2023-04-03 07:29:02 -0300

    Desenvolvido navbar e footer do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Navbar :logo="logoSrc" :alt="altSrc"/>
   <router-view/>
+  <Footer/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
+</style>
+<script>
+  import Navbar from "@/components/Navbar.vue";
+  import Footer from "@/components/Footer.vue";
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  export default {
+    components: {
+      Navbar,
+      Footer
+    },
+    data() {
+      return {
+        logoSrc: '/img/logo.png',
+        altSrc: 'Make your burger'
+      }
+    }
+  }
+</script>
+<style>
+  * {
+    font-family: "Helvetica", serif;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .main-container {
+    margin: 50px;
+    min-height: 602px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
 </style>
