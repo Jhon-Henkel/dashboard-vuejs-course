9da721832aef6e447d84721cbc7b54856026ff8f
Jhonatan Henkel <jhowrf@gmail.com>
2023-04-03 12:36:24 -0300

    Adicionado banner na home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <template>
+  <Banner/>
   <div class="main-container">
-    <h1>Home</h1>
+    <h1>Monte seu burger:</h1>
+    <p>FORMULARIO</p>
   </div>
 </template>
 
 <script>
+  import {defineComponent} from "vue";
+  import Banner from "@/components/Banner.vue";
 
+  export default defineComponent({
+    name: 'Home',
+    components: {
+      Banner,
+    }
+  })
 </script>
